6ae4f812e7e1b47fb33bed58338a3a3822c65fc9
freni226 <freni226@hotmail.com>
2015-07-20 15:15:38 +0800

    update peter 380

---
+++ zenpad_380KL/js/zenpad_380KL.js
@@ -0,0 +1,33 @@
+
+// JavaScript Document
+
+//       **************   center wrap   **************
+
+var productName = 'zenpad_380KL';    
+var productId = 'aKXBewzKRloj9LVy';  
+var product;
+
+var windowW, windowH;
+
+var Main = function() {
+ var init = function() {
+  product = $('#' + productName);
+  windowResizeInit();
+ }
+
+ function windowResizeInit() {
+  $(window).resize(function() {
+   windowW = $(window).width();
+   windowH = $(window).height();
+
+   product.css({
+    width: windowW,
+    marginLeft: -windowW / 2
+   })
+  }).resize();
+ }
+
+ return {
+  init: init
+ };
+}();
