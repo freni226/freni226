c21f5882298a68db9c2f86ca28ce90387a14d5ea
freni226 <freni226@hotmail.com>
2015-07-29 18:39:42 +0800

    update

--- zenpad_580CA/js/load.js
+++ zenpad_580CA/js/load.js
@@ -10,5 +10,6 @@ document.write('<script type="text/javascript" src="websites/global/products/'+p
 document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/mobile-detect.min.js"></script>');
 document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/detect-device.js"></script>');
 document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/movie.js"></script>');
+document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/autoplay.js"></script>');
 
 
