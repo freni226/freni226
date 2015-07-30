6b4b36c594781a0d690148910a95b4bdb15f9a55
freni226 <freni226@hotmail.com>
2015-07-30 11:06:32 +0800

    update

--- zenpad_580CA/js/autoplay.js
+++ zenpad_580CA/js/autoplay.js
@@ -5,15 +5,39 @@
 (function($) {
 	$.canboo = {
 		init:function(){
-			// autoplay
+			// 確認元素存在才執行startPlay
 			if ( $('#BLUR').length )
 			{
 				$.canboo.startPlay();
 			};
 		},
 		startPlay:function() {
-			document.getElementById('BLUR').play();
-			document.getElementById('NOBLUR').play();
+			var video1 = document.getElementById('BLUR'),
+				video2 = document.getElementById('NOBLUR');
+			video1.load();
+			video2.load();
+			// 確認兩隻影片都讀取完後，才開始執行播放
+			video1.addEventListener('loadeddata', function() {
+				video2.addEventListener('loadeddata', function() {
+					video1.currentTime = 0.5;
+					video2.currentTime = 0.5;
+					video1.play();
+					video2.play();
+				}, false);
+			}, false);
+			// 因為兩隻影片影格數有稍稍不同，所以要播完後歸零，再次啟動play
+			video1.onended = function(e) {
+				video1.currentTime = 0.5;
+				video2.currentTime = 0.5;
+				video1.play();
+				video2.play();
+		    };
+		    // 如果是行動載具，會因為無法執行自動播放程序所以要增加controls
+		    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
+		    {
+		    	video1.controls = true;
+		    	video2.controls = true;
+		    };
 		}
 	}
 
