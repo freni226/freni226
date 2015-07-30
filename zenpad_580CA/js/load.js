01ccf957b0ffc9173a6ce23eb9586823ef2e34cf
freni226 <freni226@hotmail.com>
2015-07-30 16:34:23 +0800

    update

--- zenpad_580CA/js/load.js
+++ zenpad_580CA/js/load.js
@@ -1,15 +1,14 @@
-﻿var prdID = 'aKXBewzKRloj9LVy';
+﻿var prdID = 'Z6Wda6yI8WpA2D69';
 
 //***** css  ****//
-document.write('<link href="websites/global/products/'+prdID+'/css/zenpad_580KL.css" rel="stylesheet"/>');
+document.write('<link href="websites/global/products/'+prdID+'/css/zenpad_580CA.css" rel="stylesheet"/>');
 document.write('<link href="websites/global/products/'+prdID+'/css/reset.css" rel="stylesheet"/>');
 document.write('<link href="websites/global/products/'+prdID+'/css/fonts.css" rel="stylesheet"/>');
 
 //****js****//
-document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/zenpad_580KL.js"></script>');
+document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/zenpad_580CA.js"></script>');
 document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/mobile-detect.min.js"></script>');
 document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/detect-device.js"></script>');
-document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/movie.js"></script>');
 document.write('<script type="text/javascript" src="websites/global/products/'+prdID+'/js/autoplay.js"></script>');
 
 
